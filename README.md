5209fe1089f84b52e3575ebb92a825e2c283581c
Navindu <navanavi3@gmail.com>
2022-04-10 18:33:47 +0530

    final contract

---
+++ README.md
@@ -0,0 +1,10 @@
+# $KAPUTA Token
+
+An ERC-20 Token built for no reason. $KAPUTA is currently deployed on the Rinkeby test network.
+
+## Road map
+
+[x] Yell "GoHomeGota"
+[x] $KAPUTA Token Contract
+[x] Token Sale Contract
+[] Token Sale Website
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,11 @@
+
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const privateKey = process.env.PRIV_KEY
+const infuraUrl = process.env.INFURA_URL
+
+console.log(privateKey, infuraUrl)
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -46,6 +54,18 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    // : {
+    //   provider: function () {
+    //     return new HDWalletProvider({
+    //       privateKeys: privateKey,
+    //       providerOrUrl: infuraUrl,
+    //       numberOfAddresses: 1
+    //     });
+    //   },
+    //   network_id: 4,
+    //   gas: 6700000,
+    //   gasPrice: 10000000000
+    // }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -57,14 +77,18 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        privateKeys: [privateKey],
+        providerOrUrl: infuraUrl,
+        numberOfAddresses: 1
+      }),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
